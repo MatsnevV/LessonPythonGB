eab86eeff20b4f658e23fba1510e2afa47e91f27
v.matsnev <v.matsnev@yandex.ru>
2021-01-24 14:32:19 +0300

    dz2

--- lesson 2/task2.py
+++ lesson 2/task2.py
@@ -5,3 +5,16 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input()
 """
+
+stroka_in = input()
+stroka_list = list(stroka_in)
+res = []
+
+for i in range(0, len(stroka_in) - 1, 2):
+    res.append(stroka_list[i + 1])
+    res.append(stroka_list[i])
+
+if (len(stroka_in) % 2) != 0:
+    res.append(stroka_in[-1])
+
+print(res)
