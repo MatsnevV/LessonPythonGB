fe058984eba179cb2632384dfa591015fb1f779b
v.matsnev <v.matsnev@yandex.ru>
2021-02-04 01:22:00 +0300

    dz5.2

--- lesson 5/task1.py
+++ lesson 5/task1.py
@@ -4,4 +4,11 @@
   Об окончании ввода данных свидетельствует пустая строка.
 """
 
-f_obj = open("my_file.txt")
+
+print('Введите текст')
+with open("text_task1.txt", "w") as f_obj:
+    while True:
+        znach_in = input(">")
+        print(znach_in, file=f_obj)
+        if znach_in in "\n":
+            break
--- lesson 5/task5.py
+++ lesson 5/task5.py
@@ -3,3 +3,17 @@
 Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
 
+my_f = open("text_task5.txt", "w")
+stroka = input('Введите ряд чисел через пробел " "\n>')
+content_out = my_f.write(stroka)
+my_f.close()
+
+try:
+    my_f = open("text_task5.txt", "r")
+    content_in = my_f.read()
+    count_str = sum(map(int, content_in.split(" ")))
+    print(f'Сумма составила: {count_str}')
+except IOError:
+    print("Произошла ошибка ввода-вывода!")
+finally:
+    my_f.close()
